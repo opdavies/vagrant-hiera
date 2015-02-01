50510a718328b74b629bd154de7ff6521ea7267f
Oliver Davies <oliver@oliverdavies.co.uk>
2015-02-01 21:32:34 +0000

    Added the apache vhosts directory

--- puppet/custom_modules/apache/manifests/init.pp
+++ puppet/custom_modules/apache/manifests/init.pp
@@ -1,10 +1,15 @@
 class apache {
   $apache = hiera('apache')
+  $vhosts_dir = hiera('apache::vhosts_dir')
+
   package { $apache:
     ensure => latest
   } ->
   service { $apache:
     ensure => running,
     enable => true,
+  } ->
+  file { $vhosts_dir:
+    ensure => directory,
   }
 }
